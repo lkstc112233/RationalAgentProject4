6201ec107a7dfdf8de444c72dfc50c04558adae8
Photon Cat <lkstc112233@users.noreply.github.com>
2019-04-19 19:13:18 -0400

    Rewrote the loader

--- src/com/photoncat/rationalagent/project4/util/FileLoader.java
+++ src/com/photoncat/rationalagent/project4/util/FileLoader.java
@@ -1,6 +1,8 @@
 package com.photoncat.rationalagent.project4.util;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.photoncat.rationalagent.project4.util.ExceptionHandling.panic;
 
@@ -13,100 +15,83 @@ public class FileLoader {
 	/**
 	 * Loads a graph form a stream.
 	 */
-	public static LoadedGraph load(Reader source) {
+	public static LoadedData load(Reader source) {
 		// Create a tokenizer, either by file or by text.
 		ConvenientStreamTokenizer tokens = new ConvenientStreamTokenizer(source);
 		// Compile the program and return.
-        LoadedGraph graph = null;
+        LoadedData data = null;
 		try {
-            graph = load(tokens);
+            data = load(tokens);
 		} catch (IOException e) {
 			System.err.println("File format error.");
 			System.exit(-1);
 		}
-		return graph;
+		return data;
 	}
 	/**
 	 * Alias for text source.
 	 */
-	public static LoadedGraph load(String source) {
+	public static LoadedData load(String source) {
 		return load(new StringReader(source));
 	}
 	/**
      * load a graph.
 	 */
-	private static LoadedGraph load(ConvenientStreamTokenizer tokens) throws IOException {
-        LoadedGraph graph = new LoadedGraph();
-        // Parse vertices.
+	private static LoadedData load(ConvenientStreamTokenizer tokens) throws IOException {
+        // Parse probability of not changing.
         int token = tokens.nextToken();
-        if (token != ConvenientStreamTokenizer.TT_WORD || !tokens.sval.equalsIgnoreCase("Vertices")) {
-            panic("Missing 'Vertices' field.");
+        if (token != ConvenientStreamTokenizer.TT_NUMBER) {
+            panic("Missing 'probability' field.");
         }
-        while (true) {
-            if (!parseOptionalGraphNode(tokens, graph)) break;
-        }
-        graph.sortNodes();
-        // Parse edges.
+        double probabilityOfNotSwitching = tokens.nval;
+        // Parse probability of numbers.
         token = tokens.nextToken();
-        if (token != ConvenientStreamTokenizer.TT_WORD || !tokens.sval.equalsIgnoreCase("Edges")) {
-            panic("Missing 'Edges' field.");
+        double[][] p = new double[3][];
+        for (int i = 0; i < 2; ++i) {
+            p[i] = parseProbabilityRow(tokens);
         }
-        while (true) {
-            if (!parseOptionalEdge(tokens, graph)) break;
-        }
-        graph.createEdgesCache();
-        return graph;
+        List<Integer> emissions = parseEmissions(tokens);
+        return new LoadedData(probabilityOfNotSwitching, p, emissions);
     }
     /**
      * @throws IllegalStateException when file is corrupted.
      */
-    private static boolean parseOptionalGraphNode(ConvenientStreamTokenizer tokens, LoadedGraph graph) throws IOException {
-        if (tokens.nextToken() != ConvenientStreamTokenizer.TT_NUMBER) {
-            tokens.pushBack();
-            return false;
+    private static double[] parseProbabilityRow(ConvenientStreamTokenizer tokens) throws IOException {
+        double[] row = new double[3];
+        int token = tokens.nextToken();
+        if (token != ConvenientStreamTokenizer.TT_NUMBER) {
+            panic("Missing 'probability' field.");
         }
-        int id = (int) tokens.nval;
-        if (tokens.nextToken() != ',') {
-            panic("Unexpected token at line " + tokens.lineno());
+        row[0] = tokens.nval;
+        for (int i = 1; i < 3; ++i) {
+            token = tokens.nextToken();
+            if (token != ',') {
+                panic("Invalid symbol between probabilities.");
+            }
+            token = tokens.nextToken();
+            if (token != ConvenientStreamTokenizer.TT_NUMBER) {
+                panic("Missing 'probability' field.");
+            }
+            row[i] = tokens.nval;
         }
-        if (tokens.nextToken() != ConvenientStreamTokenizer.TT_NUMBER) {
-            panic("Unexpected token at line " + tokens.lineno());
-        }
-        int x = (int) tokens.nval;
-        if (tokens.nextToken() != ',') {
-            panic("Unexpected token at line " + tokens.lineno());
-        }
-        if (tokens.nextToken() != ConvenientStreamTokenizer.TT_NUMBER) {
-            panic("Unexpected token at line " + tokens.lineno());
-        }
-        int y = (int) tokens.nval;
-        graph.addNode(id, x, y);
-        return true;
+        return row;
     }
     /**
      * @throws IllegalStateException when file is corrupted.
      */
-    private static boolean parseOptionalEdge(ConvenientStreamTokenizer tokens, LoadedGraph graph) throws IOException {
-        if (tokens.nextToken() != ConvenientStreamTokenizer.TT_NUMBER) {
-            tokens.pushBack();
-            return false;
+    private static List<Integer> parseEmissions(ConvenientStreamTokenizer tokens) throws IOException {
+        List<Integer> emissions = new ArrayList<>();
+        // Parse the emissions array
+        int token = tokens.nextToken();
+        if (token != '[') {
+            panic("Missing 'emissions' field.");
         }
-        int from = (int) tokens.nval;
-        if (tokens.nextToken() != ',') {
-            panic("Unexpected token at line " + tokens.lineno());
+        while (tokens.nextToken() == ConvenientStreamTokenizer.TT_NUMBER) {
+            emissions.add((int) tokens.nval);
+            if (tokens.nextToken() == ']') {
+                break;
+            }
         }
-        if (tokens.nextToken() != ConvenientStreamTokenizer.TT_NUMBER) {
-            panic("Unexpected token at line " + tokens.lineno());
-        }
-        int to = (int) tokens.nval;
-        if (tokens.nextToken() != ',') {
-            panic("Unexpected token at line " + tokens.lineno());
-        }
-        if (tokens.nextToken() != ConvenientStreamTokenizer.TT_NUMBER) {
-            panic("Unexpected token at line " + tokens.lineno());
-        }
-        int distance = (int) tokens.nval;
-        graph.addEdge(from, to, distance);
-        return true;
+        return emissions;
     }
 }
