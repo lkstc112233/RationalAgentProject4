cfa32d5031a130bf889115592da988d66b86ce7e
Photon Cat <lkstc112233@users.noreply.github.com>
2019-04-21 00:07:44 -0400

    Implemented Viterbi Algorithm

--- src/com/photoncat/rationalagent/project4/CommandLineMain.java
+++ src/com/photoncat/rationalagent/project4/CommandLineMain.java
@@ -22,5 +22,46 @@ public class CommandLineMain {
             e.printStackTrace();
         }
         // TODO: handle the file.
+        double[] probabilityLastLayer = null;
+        int[][] fromWhere = new int[data.getLength()][3];
+        int currentIndex = 0;
+        int largestLastLayer = 0;
+        final double INITIAL_PROBABILITY = 1.0 / 3;
+        for (Integer observation: data) {
+            if (probabilityLastLayer == null) {
+                probabilityLastLayer = new double[3];
+                for (int i = 0; i < 3; ++i) {
+                    probabilityLastLayer[i] = INITIAL_PROBABILITY * data.getProbabilityOfRolling(i + 1, observation);
+                }
+            } else {
+                double[][] newProbability = new double[3][3];
+                for (int i = 0; i < 3; ++i) {
+                    for (int j = 0; j < 3; ++j) {
+                        double pTransfer =
+                                i == j ? data.getProbabilityOfNotSwitching()
+                                        : ((1 - data.getProbabilityOfNotSwitching()) / 2);
+                        newProbability[i][j] = probabilityLastLayer[i] * pTransfer * data.getProbabilityOfRolling(j + 1, observation);
+                    }
+                }
+                double globalMax = 0;
+                for (int i = 0; i < 3; ++i) {
+                    double max = 0;
+                    int maxId = 0;
+                    for (int j = 0; j < 3; ++j) {
+                        if (newProbability[i][j] > max) {
+                            max = newProbability[i][j];
+                            maxId = j;
+                        }
+                    }
+                    probabilityLastLayer[i] = max;
+                    fromWhere[currentIndex][i] = maxId;
+                    if (globalMax < max) {
+                        globalMax = max;
+                        largestLastLayer = i;
+                    }
+                }
+            }
+            currentIndex += 1;
+        }
     }
 }
